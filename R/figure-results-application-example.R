c99d138fbf350e9888b3b9aa5dcd03ba451a1581
Marta Karas <martakaras@macbook-pro-6.home>
2019-06-30 22:17:16 +0200

    Add code to generate Figure 14: Individual strides segmented with ADEPT together with the derived subject- and location-specific stride pattern for three study participants, across four sensor locations.

--- R/figure-results-application-example.R
+++ R/figure-results-application-example.R
@@ -4,12 +4,17 @@
 #' 
 #' @description 
 #' Code to generate manuscript figures: 
+#' - Figure 10: Sensor location-specific sets of estimated empirical patterns of 
+#'   a stride. 
 #' - Figure 11: Heatmap  of  walking  cadence  estimates  using  ADEPT  on  left  
 #'   ankle  VM  data. 
 #' - Figure 12: Estimated start point (x-axis) and duration time (y-axis) of all 
 #'   estimated strides for onestudy participant across four sensor locations. 
 #' - Figure 13: Comparison of stride segmentation using the manual and ADEPT 
 #'   approaches for four different body locations.
+#' - Figure 14: Individual strides segmented with ADEPT together with the derived 
+#'   subject- and location-specific stride pattern for three study participants, 
+#'   across four sensor locations.
 
 
 rm(list = ls())
@@ -25,6 +30,48 @@ source(file.path("R", "util.R"))
 ggsave.device  <- "png"
 loc_id.levels  <- c("left_wrist", "left_hip","left_ankle", "right_ankle")
 loc_id.labels  <- c( "Left wrist","Left hip", "Left ankle", "Right ankle")
+
+
+## -----------------------------------------------------------------------------
+
+## Figure 10: 
+## Sensor location-specific sets of estimated empirical patterns of a stride. 
+plt.name <- "results_estimated_empirical_patterns.png"
+
+vl <- 200
+plt.df <- data.frame(
+  y = c(stride_template$left_wrist[[2]][1, ], 
+        stride_template$left_wrist[[2]][2, ],
+        stride_template$left_hip[[2]][1, ],   
+        stride_template$left_hip[[2]][2, ],
+        stride_template$left_ankle[[2]][1, ],
+        stride_template$left_ankle[[2]][2, ],
+        stride_template$right_ankle[[2]][1, ],
+        stride_template$right_ankle[[2]][2, ]),
+  x = as.vector(replicate(8, seq(0, 1, length.out = vl))),
+  loc_id = as.vector(sapply(loc_id.labels, function(loc_id.tmp) rep(loc_id.tmp, 2 * vl))),
+  cluster_id = as.vector(replicate(4, c(rep(1, vl), rep(2, vl)))),
+  stringsAsFactors = FALSE
+)
+plt.df$loc_id <- factor(plt.df$loc_id, levels = loc_id.labels)
+plt.df$cluster_id <- factor(
+  plt.df$cluster_id, 
+  levels = c(1,2), 
+  labels = paste0("Empirical pattern ", c(1,2)))
+
+plt <- 
+  ggplot(plt.df, aes(x = x, y = y, group = 1, color = loc_id)) + 
+  geom_hline(yintercept = 0, linetype = 2, color = "darkgrey") + 
+  geom_line(size = 1) + 
+  facet_grid(cluster_id ~ loc_id) +
+  theme_Publication() + 
+  labs(x = "Stride phase", 
+       y = "Vector magnitude [g]", 
+       color = "Sensor location") + 
+  theme(legend.position="none") 
+# plot(plt)
+ggsave(filename = file.path("figures", plt.name), plot = plt, device = ggsave.device, 
+       width = 10, height = 4.2, units = "in")
 
 
 ## -----------------------------------------------------------------------------
@@ -292,8 +339,116 @@ ggsave(filename = file.path("figures", plt.name), plot = plt, device = ggsave.de
 
 
 
+## -----------------------------------------------------------------------------
 
+## Figure 14: 
+## Individual strides segmented with ADEPT together with the derived subject- 
+## and location-specific stride pattern for three study participants, 
+## across four sensor locations.
 
+plt.name <- "results_individual_strides_3_participants.png"
 
+## Participants subset 
+subj_id.sub.idx <- which(subj_id.labels %in% c("1", "18", "32")) 
+subj_id.levels.sub <- subj_id.levels[subj_id.sub.idx]
+subj_id.labels.sub <- subj_id.labels[subj_id.sub.idx]
+
+## Accelerometry data subset filtered to keep only data of three participants
+acc.sub <- 
+  acc_walking_IU %>% 
+  filter(subj_id %in% subj_id.levels.sub) %>%
+  group_by(subj_id, loc_id) %>%
+  arrange(time_s) %>%
+  mutate(tau_i = row_number(),
+         vm = sqrt(x^2 + y^2 + z^2)) %>%
+  group_by() %>%
+  arrange(loc_id, time_s) %>%
+  as.data.frame()
+
+## Objects to store subject- and location-specific stride pattern estimates
+subj_id.vec    <- numeric()
+loc_id.vec     <- numeric()
+phase.vec      <- numeric()
+val.vec        <- numeric()
+obs_idx.vec    <- numeric()
+
+itpl.n <- 200
+
+for (subj_id.tmp in subj_id.levels.sub){   
+  for (loc_id.tmp in loc_id.levels){  
+    # subj_id.tmp <-  subj_id.levels.sub[1]; loc_id.tmp <- loc_id.levels[1]
+    message(paste0("subj_id: ", subj_id.tmp, ", loc_id: ", loc_id.tmp))
+    
+    ## Subject- and location-specific subset of raw acc data 
+    acc.sub.subj_loc <- acc.sub %>% 
+      filter(subj_id == subj_id.tmp, loc_id == loc_id.tmp) %>%
+      arrange(tau_i)
+    ## Subject- and location-specific subset of segmentation results
+    res.subj_loc <- res.df %>% 
+      filter(subj_id == subj_id.tmp, loc_id == loc_id.tmp)
+
+    ## Skip first and last slide of a person
+    for (i in 2:(nrow(res.subj_loc)-1)){ 
+      # i <- 2
+      tau_i.tmp  <- res.subj_loc$tau_i[i]
+      T_i.tmp    <- res.subj_loc$T_i[i]
+      vm_idx.tmp <- tau_i.tmp : (tau_i.tmp + T_i.tmp - 1)
+      vm.tmp     <- acc.sub.subj_loc$vm[vm_idx.tmp]
+      vm.approx.tmp <- (approx(
+        x = seq(0, 1, length.out = length(vm.tmp)),
+        y = vm.tmp,
+        xout = seq(0, 1, length.out = itpl.n)
+      ))$y
+        
+      ## Store results
+      subj_id.vec    <- c(subj_id.vec, rep(subj_id.tmp, itpl.n))
+      loc_id.vec     <- c(loc_id.vec,  rep(loc_id.tmp, itpl.n))
+      phase.vec      <- c(phase.vec, seq(0, 1, length.out = itpl.n))
+      val.vec        <- c(val.vec, vm.approx.tmp)
+      obs_idx.vec    <- c(obs_idx.vec, rep(i, itpl.n))
+    }
+  }
+}
+
+## Put results into data frame, format data frame
+plt.df <- data.frame(
+  subj_id = subj_id.vec, 
+  loc_id  = loc_id.vec, 
+  phase   = phase.vec, 
+  val     = val.vec, 
+  obs_idx = obs_idx.vec, 
+  stringsAsFactors = FALSE)
+plt.df$subj_id <- factor(
+  as.character(plt.df$subj_id),
+  levels = rev(subj_id.levels.sub),
+  labels = rev(paste0("Paerticipant ", subj_id.labels.sub)))
+plt.df$loc_id <- factor(
+  as.character(plt.df$loc_id),
+  levels = loc_id.levels,
+  labels = loc_id.labels)
+
+## Aggregate values to get estimated stride patterns
+plt.df.agg <- 
+  plt.df %>% 
+  group_by(subj_id, loc_id, phase) %>%
+  summarize(val_mean = mean(val),
+            val_median = median(val))
+
+plt <- 
+  ggplot(plt.df, aes(x = phase, y = val, color = loc_id, group = obs_idx)) + 
+  geom_line(alpha = 0.05) + 
+  geom_line(data = plt.df.agg,  
+            aes(x = phase, y = val_mean, group = 1), 
+            color = "black", inherit.aes = FALSE) +
+  facet_grid(subj_id ~ loc_id) + 
+  theme_Publication() + 
+  labs(title = "", 
+       x = "Stride phase", 
+       y = "Vector magnitude [g]") + 
+  theme(legend.position = 'none') 
+# plot(plt)
+
+ggsave(filename = file.path("figures", plt.name), plot = plt, device = ggsave.device,
+       width = 10, height = 6, units = "in")
 
 
